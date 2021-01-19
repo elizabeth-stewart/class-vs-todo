75dd89de8561c02ee9471b84869421c3cf9245b2
Elizabeth Stewart <estewart@dynatronsoftware.com>
2021-01-18 19:57:07 -0600

    Hooking up front end with Hello World Service

--- front-end/todo/src/app/welcome/welcome.component.ts
+++ front-end/todo/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,14 +8,48 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  welcomeMessageFromService:string
   name = ''
 
   // inject username into routing
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) { }
 
   ngOnInit() {
     // snapshot = list of parameters
     this.name = this.route.snapshot.params['name']
   }
 
+  getWelcomeMessage() {
+    // console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+    console.log("last line of get welcome message");
+  }
+
+  getWelcomeMessageWithParameter() {
+    // console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+    console.log("last line of get welcome message");
+  }
+
+
+  handleErrorResponse(error: any): void {
+    // console.log(error);
+    // console.log(error.error);
+    // console.log(error.error.message);
+    this.welcomeMessageFromService = error.error.message;
+  
+  }
+
+  handleSuccessfulResponse(response) {
+    this.welcomeMessageFromService = response.message;
+    // console.log(response.message);
+  }
 }
