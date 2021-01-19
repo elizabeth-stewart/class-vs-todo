2765a09066673ba5639b9d412094a19d1ff3850b
Elizabeth Stewart <estewart@dynatronsoftware.com>
2021-01-18 19:58:02 -0600

    Adding cross-origin request

--- back-end/restful-web-services/src/main/java/com/bearcat/rest/webservices/restfulwebservices/HelloWorldController.java
+++ back-end/restful-web-services/src/main/java/com/bearcat/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.bearcat.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
     // GET
@@ -17,7 +19,8 @@ public class HelloWorldController {
     // return bean
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World");
+        return new HelloWorldBean("Hello World - Changed");
+//        throw new RuntimeException("Some error has happened!  Contact Support at ***-****");
     }
 
     @GetMapping(path = "/hello-world/path-variable/{name}")
