9be34e71dc5e35f97b67f8b0ecd902ae386834ef
Elizabeth Stewart <estewart@dynatronsoftware.com>
2021-01-31 15:10:21 -0600

    Retrieve one Todo

--- back-end/restful-web-services/src/main/java/com/bearcat/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ back-end/restful-web-services/src/main/java/com/bearcat/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -13,11 +13,30 @@ public class TodoHardcodedService {
 
     static {
         todos.add(new Todo(++idCounter, "templeton", "Learn to Dance", new Date(), false));
-        todos.add(new Todo(++idCounter, "templeton", "Learn about Microservices", new Date(), false));
+        todos.add(new Todo(++idCounter, "templeton", "Learn about Microservices 2", new Date(), false));
         todos.add(new Todo(++idCounter, "templeton", "Learn about Angular", new Date(), false));
     }
 
     public List<Todo> findAll() {
         return todos;
+    }
+
+    public Todo deleteById(long id) {
+        Todo todo = findById(id);
+
+        if (todo == null) return null;
+
+        if (todos.remove(todo)) {
+            return todo;
+        }
+
+        return null;
+    }
+
+    public Todo findById(long id) {
+        return todos.stream()
+                .filter(todo -> todo.getId() == id)
+                .findFirst()
+                .orElse(null);
     }
 }
