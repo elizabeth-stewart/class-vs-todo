f5be98abf648375187a036d22098df0fb26fd4f8
Elizabeth Stewart <estewart@dynatronsoftware.com>
2021-01-31 18:39:40 -0600

    PUT and POST Todo

--- back-end/restful-web-services/src/main/java/com/bearcat/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ back-end/restful-web-services/src/main/java/com/bearcat/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,9 +1,12 @@
 package com.bearcat.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -20,6 +23,31 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos/{id}")
     public Todo getTodo(@PathVariable String username, @PathVariable long id) {
         return todoService.findById(id);
+    }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username,
+                                           @PathVariable long id,
+                                           @RequestBody Todo todo) {
+        Todo updatedTodo = todoService.save(todo);
+
+        // could just return a Todo object, but returning a ResponseEntity gives more options
+        // in case we want to return some other responses for errors
+        return new ResponseEntity<>(updatedTodo, HttpStatus.OK);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<Void> addTodo(@PathVariable String username,
+                                          @RequestBody Todo todo) {
+        Todo addedTodo = todoService.save(todo);
+
+        // want to return location of new resource
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(addedTodo.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
     @DeleteMapping("/users/{username}/todos/{id}")
